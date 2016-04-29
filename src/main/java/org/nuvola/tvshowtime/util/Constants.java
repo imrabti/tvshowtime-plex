9081b1774336d02a14525984fddccb86c7a456c3
imrabti <idriss.mrabti@intelcia.com>
2016-04-29 15:51:41 +0200

    Simplify the configuration file, hide some technical details that the user shouldn't see and change.

---
+++ src/main/java/org/nuvola/tvshowtime/util/Constants.java
@@ -0,0 +1,12 @@
+package org.nuvola.tvshowtime.util;
+
+public class Constants {
+    public static final String TVST_CLIENT_ID = "va0D2CEfSPNNlLoYMYYT";
+    public static final String TVST_CLIENT_SECRET = "RF51gSEZBJAbLXmEUCZ8thJAwJPAyQSafCQCyqOt";
+    public static final String TVST_USER_AGENT = "plex-tvst-scrobbler";
+    public static final String TVST_AUTHORIZE_URI = "https://api.tvshowtime.com/v1/oauth/device/code";
+    public static final String TVST_ACCESS_TOKEN_URI = "https://api.tvshowtime.com/v1/oauth/access_token";
+    public static final String TVST_CHECKIN_URI = "https://api.tvshowtime.com/v1/checkin";
+
+    public static final String PMS_WATCH_HISTORY = "/status/sessions/history/all";
+}
