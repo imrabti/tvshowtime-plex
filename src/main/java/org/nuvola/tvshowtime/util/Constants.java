99373f7f8c6357251a3a7573897fa1f5949439d3
imrabti <imrabti@gmail.com>
2016-05-27 06:44:02 +0100

    1. Upgrade Spring Boot to 1.3.5. 2. Update TVShowTime API clientID and clientSecret.

--- src/main/java/org/nuvola/tvshowtime/util/Constants.java
+++ src/main/java/org/nuvola/tvshowtime/util/Constants.java
@@ -19,9 +19,9 @@
 package org.nuvola.tvshowtime.util;
 
 public class Constants {
-    public static final String TVST_CLIENT_ID = "va0D2CEfSPNNlLoYMYYT";
-    public static final String TVST_CLIENT_SECRET = "RF51gSEZBJAbLXmEUCZ8thJAwJPAyQSafCQCyqOt";
-    public static final String TVST_USER_AGENT = "plex-tvst-scrobbler";
+    public static final String TVST_CLIENT_ID = "UOWED7wBGRQv17skSZJO";
+    public static final String TVST_CLIENT_SECRET = "ZHYcO8n8h6WbYuMDWVgXr7T571ZF_s1r1Rzu1-3B";
+    public static final String TVST_USER_AGENT = "tvshowtime-plex";
     public static final String TVST_AUTHORIZE_URI = "https://api.tvshowtime.com/v1/oauth/device/code";
     public static final String TVST_ACCESS_TOKEN_URI = "https://api.tvshowtime.com/v1/oauth/access_token";
     public static final String TVST_CHECKIN_URI = "https://api.tvshowtime.com/v1/checkin";
